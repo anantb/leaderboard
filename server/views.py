28a7b53464fd604361beda793937edeb7a7bc7a4
Anant Bhardwaj <anantb@csail.mit.edu>
2013-09-23 07:47:22 -0400

    error handling -- revert

--- server/views.py
+++ server/views.py
@@ -165,9 +165,8 @@ def scores(request):
 
 
 @login_required
-def home(request, errors = None):
-    c = {'errors': errors}
-    c.update(csrf(request))
+def home(request):
+    c = csrf(request)
     return render_to_response('home.html', c)
 
 @login_required
@@ -189,7 +188,9 @@ def upload(request):
     except:
         msg = sys.exc_info()[1]
         errors.append(msg)
-        return HttpResponseRedirect(errors)
+        c = {'errors': errors}
+        c.update(csrf(request))
+        return render_to_response('home.html', c)
 
 
 
