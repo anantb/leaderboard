f22d67576a52982ad6ce26c3d6f84f1a7ef08813
Anant Bhardwaj <anantb@csail.mit.edu>
2013-09-23 01:33:55 -0400

    more changes

--- server/settings.py
+++ server/settings.py
@@ -60,7 +60,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/lb_static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
