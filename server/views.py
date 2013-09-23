2e8bb6d2c2852173f2f7e3fb4240f0adde481784
Anant Bhardwaj <anantb@csail.mit.edu>
2013-09-23 19:53:49 -0400

    0 precision

--- server/views.py
+++ server/views.py
@@ -182,22 +182,19 @@ def compute_score(file_name):
     correct_count = 0
     precision = 0.0
     recall = 0.0
-    try:
-        f = open( p + '/user_uploads/' + file_name, 'rU')
-        reader = csv.reader(f)
-        reader.next()
-        for row in reader:
-            count += 1
-            try:
-                if(gold[row[0]] == row[1]):
-                    correct_count += 1
-            except:
-                pass
-    
-        precision = float(correct_count) / count
-        recall = float(correct_count) / len(gold.keys())
-    except:
-        pass
+    f = open( p + '/user_uploads/' + file_name, 'rU')
+    reader = csv.reader(f)
+    reader.next()
+    for row in reader:
+        count += 1
+        try:
+            if(gold[row[0]] == row[1]):
+                correct_count += 1
+        except:
+            pass
+
+    precision = float(correct_count) / count
+    recall = float(correct_count) / len(gold.keys())
     return [precision, recall]
 
 
